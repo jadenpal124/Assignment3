ea506d7735058f4ecdd42048b8dfb902771cc41c
jadenpal124 <130260991+jadenpal124@users.noreply.github.com>
2024-07-04 01:51:15 -0700

    Add files via upload

---
+++ controlModule.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "product.h"
+#include "user.h"
+#include "changeRequest.h"
+#include "release.h"
+#include "changeItem.h"
+
+using namespace std;
+
+void handleAddProduct() {
+        cout << "Executing 'Add Product' operation..." << endl;
+        // Add your code for 'Add Product' operation here
+    }
+
+    void handleAddRelease() {
+        cout << "Executing 'Add Release' operation..." << endl;
+        // Add your code for 'Add Release' operation here
+    }
+
+    void handleAddChangeRequest() {
+        cout << "Executing 'Add Change Request' operation..." << endl;
+        // Add your code for 'Add Change Request' operation here
+    }
+
+    void handleUpdateUser() {
+        cout << "Executing 'Update User' operation..." << endl;
+        // Add your code for 'Update User' operation here
+    }
+
+    void handleUpdateSingleIssue() {
+        cout << "Executing 'Update Single Issue' operation..." << endl;
+        // Add your code for 'Update Single Issue' operation here
+    }
+
+    void handleRemainingItemsReport() {
+        cout << "Executing 'Remaining Items to be Implemented Report' operation..." << endl;
+        // Add your code for 'Remaining Items Report' operation here
+    }
+
+    void handleUsersToBeNotifiedReport() {
+        cout << "Executing 'Users to be Notified Report' operation..." << endl;
+        // Add your code for 'Users to be Notified Report' operation here
+    }
+
+    void handleStatusOfChangeItems() {
+        cout << "Executing 'Status of Change Items' operation..." << endl;
+        // Add your code for 'Status of Change Items' operation here
+    }
--- ui.cpp
+++ ui.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "controlModule.cpp"
 using namespace std;
 
 class UIModule {
@@ -135,45 +136,5 @@ private:
             }
             
         } while (choice != 0);
-    }
-
-    void handleAddProduct() {
-        cout << "Executing 'Add Product' operation..." << endl;
-        // Add your code for 'Add Product' operation here
-    }
-
-    void handleAddRelease() {
-        cout << "Executing 'Add Release' operation..." << endl;
-        // Add your code for 'Add Release' operation here
-    }
-
-    void handleAddChangeRequest() {
-        cout << "Executing 'Add Change Request' operation..." << endl;
-        // Add your code for 'Add Change Request' operation here
-    }
-
-    void handleUpdateUser() {
-        cout << "Executing 'Update User' operation..." << endl;
-        // Add your code for 'Update User' operation here
-    }
-
-    void handleUpdateSingleIssue() {
-        cout << "Executing 'Update Single Issue' operation..." << endl;
-        // Add your code for 'Update Single Issue' operation here
-    }
-
-    void handleRemainingItemsReport() {
-        cout << "Executing 'Remaining Items to be Implemented Report' operation..." << endl;
-        // Add your code for 'Remaining Items Report' operation here
-    }
-
-    void handleUsersToBeNotifiedReport() {
-        cout << "Executing 'Users to be Notified Report' operation..." << endl;
-        // Add your code for 'Users to be Notified Report' operation here
-    }
-
-    void handleStatusOfChangeItems() {
-        cout << "Executing 'Status of Change Items' operation..." << endl;
-        // Add your code for 'Status of Change Items' operation here
     }
 };
