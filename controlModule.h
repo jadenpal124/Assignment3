8aa4d212a304c13798b56b3d0dc506e0ab759249
jadenpal124 <130260991+jadenpal124@users.noreply.github.com>
2024-07-04 21:28:13 -0700

    Update and rename controlModule.cpp to controlModule.h

---
+++ controlModule.h
@@ -0,0 +1,79 @@
+/* Revision History: 
+   Rev. 1 - 04/07/24 Original by JAWS */
+//==================================================
+/* This module defines functions for handling various operations related to product management,
+   user updates, change requests, and reports within the system. */
+//==================================================
+/* Include Statements */
+#ifndef CONTROL_MODULE_H
+#define CONTROL_MODULE_H
+
+#include <iostream>
+#include "product.h"
+#include "user.h"
+#include "changeRequest.h"
+#include "release.h"
+#include "changeItem.h"
+
+using namespace std;
+
+//==================================================
+
+// Function prototypes for operations handled by the control module
+
+void AddProduct();
+// Parameters: None
+// Returns: void
+// Description:
+//   Executes 'Add Product' operation.
+
+//--------------------
+void AddRelease();
+// Parameters: None
+// Returns: void
+// Description:
+//   Executes 'Add Release' operation.
+
+//--------------------
+void AddChangeRequest();
+// Parameters: None
+// Returns: void
+// Description:
+//   Executes 'Add Change Request' operation.
+
+//--------------------
+void updateUser();
+// Parameters: None
+// Returns: void
+// Description:
+//   Executes 'Update User' operation.
+
+//--------------------
+void  updateChangeItem();
+// Parameters: None
+// Returns: void
+// Description:
+//   Executes 'Update Single Issue' operation.
+
+//--------------------
+void displayRemReport();
+// Parameters: None
+// Returns: void
+// Description:
+//   Executes 'Remaining Items Report' operation.
+
+//--------------------
+void displayNotifyReport();
+// Parameters: None
+// Returns: void
+// Description:
+//   Executes 'Users to be Notified Report' operation.
+
+//--------------------
+void getStatus();
+// Parameters: None
+// Returns: void
+// Description:
+//   Executes 'Status of Change Items' operation.
+
+#endif // CONTROL_MODULE_H
