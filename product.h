d30f9e40ebbb11c0b822199ef6aa7e2f5a4b1a01
jadenpal124 <130260991+jadenpal124@users.noreply.github.com>
2024-07-05 14:19:00 -0700

    Add files via upload

--- product.h
+++ product.h
@@ -1,105 +1,86 @@
-//Revision History: 
-//Rev. 1 - 04/07/24 Original by JAWS
-//==================================================
-//This module is the implementation of the Product class. 
-//User class contains all the data about user and functions to access them.
-//==================================================
+/* Revision History: 
+   Rev. 1 - 04/07/24 Original by JAWS */
+//================================ 
+/* This module implements the Product class, which encapsulates data related to a product.
+   The class provides methods to access, modify, and manage product information,
+   demonstrating the principles of abstraction and high cohesion by encapsulating
+   related functionality within the Product class. */
+//================================ 
 #ifndef PRODUCT_H
 #define PRODUCT_H
 
 #include <iostream>
 #include <cstring> // For string manipulation functions
-
 using namespace std;
-//==================================================
-class Product {
-public: 
-    // Constructors
-    Product(); // Default constructor initializing productID and name to empty strings.
 
-    Product(const char* productID, const char* name); // Parameterized constructor
-    // Description: Parameterized constructor initializing productID and name with provided values.
+//================================
+class Product {
+
+public:
+    
+    // Constructors
+    //----------------------
+    Product (); // Default constructor
+
+    //----------------------
+    Product (const char* productID, const char* name); // Parameterized constructor
+    // Parameterized constructor initializing productID and name with provided values.
     // Parameters:
     //   - productID: Pointer to a character array containing the product ID (input)
     //   - name: Pointer to a character array containing the product name (input)
 
     // Getter methods
     //----------------------
-    const char* getProductID() const;
-    // Description: Retrieves the product ID.
-    // Parameters: None
-    // Exceptions: None.
+
+    const char* getProductID () const;
+    // Retrieves the product ID.
+    // Parameters: None (no parameters)
 
     //----------------------
-    const char* getName() const;
-    // Description: Retrieves the product name.
-    // Parameters: None
-    // Exceptions: None.
+    const char* getName () const;
+    // Retrieves the product name.
+    // Parameters: None (no parameters)
 
     // Setter methods
     //----------------------
-    void setProductID(const char* productID);
-    // Description: Sets the product ID.
+
+    void setProductID (const char* productID);
+    // Sets the product ID.
     // Parameters:
     //   - productID: Pointer to a character array containing the product ID (input)
-    // Exceptions: None.
 
     //----------------------
-    void setName(const char* name);
-    // Description: Sets the product name.
+    void setName (const char* name);
+    // Sets the product name.
     // Parameters:
     //   - name: Pointer to a character array containing the product name (input)
-    // Exceptions: None.
+
 
     // Utility methods
     //----------------------
-    bool checkUser(const char* fileName, const char* productIDToFind); 
-    // Description: Checks if a product with the given ID exists in a file.
+
+    User& checkUserExists (const char* fileName, const char* productIDToFind); 
+    // Description: Checks if a user with the given ID exists in a file.
     // Parameters:
     //   - fileName: Pointer to a character array containing the file name (input)
-    //   - productIDToFind: Pointer to a character array containing the product ID to search for (input)
-    // Exceptions: None.
+    //   - productIDToFind: Pointer to a character array containing the user ID to search for (input)
+    // Returns:
+    //   - Reference to the User object if the user with the given ID exists in the file.
+    // Exceptions: May throw an exception if the file specified by fileName does not exist or cannot be accessed.
 
     //----------------------
-    bool addProduct(const char* fileName);
-    // Description: Adds a product to a file.
+    bool addProduct (const char* fileName); 
+    // Description: Creates a product and adds it to the file.
     // Parameters:
-    //   - fileName: Pointer to a character array containing the file name (input)
-    // Exceptions: None.
-
-    //----------------------
-    bool checkaddProduct(const char* fileName, int productID); 
-    // Description: Method to check if added product with the given ID in the given file has been successfully added.
-    // Parameters:
-    //   - fileName: Pointer to a character array containing the file name (input)
-    //   - productID: Integer ID of the change item to find (input)
-    // Exceptions: None.
-
-    //----------------------
-    bool checkProduct(const char* fileName, int productID); 
-    // Description: Method to check if product with the given ID in the given file
-    // Parameters:
-    //   - fileName: Pointer to a character array containing the file name (input)
-    //   - productID: Integer ID of the change item to find (input)
-    // Exceptions: None.
-
-    //----------------------
-    void initProduct(const char* productID, const char* name);
-    // Description: Initializes the product with the provided values.
-    // Parameters:
-    //   - productID: Pointer to a character array containing the product ID (input)
-    //   - name: Pointer to a character array containing the product name (input)
-    // Exceptions: None.
-
-    void closeProduct();
-    // Description: save the product and free the memory.
-    // Parameters: None (no parameters)
-    // Exceptions: None.
+    //   - fileName: Pointer to a character array containing the file name (input/output)
+    // Returns:
+    //   - true if the product is successfully added to the file, false otherwise.
+    // Exceptions: May throw an exception if the file specified by fileName does not exist or cannot be accessed.
 
 private:
     char productID[8]; // Member variable for storing product ID (max length: 8 characters)
     char name[30]; // Member variable for storing product name (max length: 30 characters)
 };
-//==================================================
+//================================
 
 #endif // PRODUCT_H
